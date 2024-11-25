ce9b57a8b065af1f54926a668e3b5f301c793cea
Victoria Aleksandrova <vicaleksan@gmail.com>
2024-11-25 20:14:35 +0100

    Add PasswordView mobile markup

--- src/components/PasswordVeiw/index.jsx
+++ src/components/PasswordVeiw/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import styles from "./style.module.css";
+import copyIcon from "/src/images/icon-copy.svg";
 
 function PasswordVeiw() {
     return (
         <div className={styles.container}>
             <p className={styles.password}>PTx1f5DaFX</p>
+            <button className={styles.btn_copy}>
+                <img
+                    className={styles.copyIcon}
+                    src={copyIcon}
+                    alt="Copy password"
+                />
+            </button>
         </div>
     )
 }
