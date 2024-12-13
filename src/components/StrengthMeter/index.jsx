ff87cfec346d5fdb72692b2249c75784620ef894
Victoria Aleksandrova <vicaleksan@gmail.com>
2024-12-13 11:03:52 +0100

    Add clsx

--- src/components/StrengthMeter/index.jsx
+++ src/components/StrengthMeter/index.jsx
@@ -1,31 +1,8 @@
 import React from "react";
 import styles from "./style.module.css";
+import clsx from 'clsx';
 
 function StrengthMeter({ passwordComplexity }) {
-    let complexityStyle;
-    const arr = [false, false, false, false];
-    if (passwordComplexity === "Too weak!") {
-        complexityStyle = styles.tooWeak;
-        arr[0] = true;
-    }
-    if (passwordComplexity === "Weak") {
-        complexityStyle = styles.weak;
-        arr[0] = true;
-        arr[1] = true;
-    }
-    if (passwordComplexity === "Medium") {
-        complexityStyle = styles.medium;
-        arr[0] = true;
-        arr[1] = true;
-        arr[2] = true;
-    }
-    if (passwordComplexity === "Strong") {
-        complexityStyle = styles.strong;
-        arr[0] = true;
-        arr[1] = true;
-        arr[2] = true;
-        arr[3] = true;
-    }
 
     return (
         <div className={styles.strengthMeter_container}>
@@ -33,10 +10,32 @@ function StrengthMeter({ passwordComplexity }) {
             <div className={styles.passwordStrength}>
                 {passwordComplexity}
                 <div className={styles.passwordStrength_visual}>
-                    <div className={`${styles.passwordStrength_visual_block} ${arr[0] ? complexityStyle : ""}`}></div>
-                    <div className={`${styles.passwordStrength_visual_block} ${arr[1] ? complexityStyle : ""}`}></div>
-                    <div className={`${styles.passwordStrength_visual_block} ${arr[2] ? complexityStyle : ""}`}></div>
-                    <div className={`${styles.passwordStrength_visual_block} ${arr[3] ? complexityStyle : ""}`}></div>
+                    <div className={clsx(styles.passwordStrength_visual_block, {
+                        [styles.tooWeak] : passwordComplexity === "Too weak!",
+                        [styles.weak] : passwordComplexity === "Weak",
+                        [styles.medium] : passwordComplexity === "Medium",
+                        [styles.strong] : passwordComplexity === "Strong"
+                        })}
+                    >
+                    </div>
+                    <div className={clsx(styles.passwordStrength_visual_block, {
+                        [styles.weak] : passwordComplexity === "Weak",
+                        [styles.medium] : passwordComplexity === "Medium",
+                        [styles.strong] : passwordComplexity === "Strong"
+                        })}
+                    >
+                    </div>
+                    <div className={clsx(styles.passwordStrength_visual_block, {
+                        [styles.medium] : passwordComplexity === "Medium",
+                        [styles.strong] : passwordComplexity === "Strong"
+                        })}
+                    >
+                    </div>
+                    <div className={clsx(styles.passwordStrength_visual_block, {
+                        [styles.strong] : passwordComplexity === "Strong"
+                        })}
+                    >
+                    </div>
                 </div>
             </div>
         </div>
